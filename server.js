f1156cd38c3b25b042fae540c780913280ada58b
Terrence Celestine <terrencebutler89@gmail.com>
2021-12-02 17:51:56 -0800

    Connected app to mongodb
    - Uses env variables to hide credentials.
    - Localhost uses local variables

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+// only use .env variables if not running on production
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const port = 3001;
+
+app.use(express.json());
+
+const user = process.env.USER;
+const password = process.env.PASSWORD;
+const db_uri = process.env.DB_URI;
+
+mongoose.connect(`mongodb+srv://${user}:${password}@${db_uri}`);
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("CONNECTED SUCCESSFULLY");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
